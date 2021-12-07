355e8ea6ba34ee123b8c4d8a8b65a2eb23ef23d4
Jeffrey <jsrimr@naver.com>
2021-12-07 17:41:38 +0900

    starting to experiment incremental scaling

--- net_transform.py
+++ net_transform.py
@@ -14,10 +14,10 @@ import neptune.new as neptune
 FROM_SCRATCH = True
 from models.mobilenetv2 import MobileNetV2
 
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+start_epoch = 1  # start from epoch 0 or last checkpoint epoch
+total_epoch = 201
 
 # Data
 print('==> Preparing data..')
@@ -46,9 +46,21 @@ testloader = torch.utils.data.DataLoader(
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
-from utils import train, test, BASE_CFG
+from utils import get_next_net, train, test, BASE_CFG
 
 if __name__ == '__main__':
+
+    PLAN = [
+        ("width", 2),
+        ("depth", 2),
+        ("width", 3),
+        ("depth", 3),
+        ("width", 4),
+        ("depth", 4),
+        ("width", 5),
+        ("depth", 5),
+    ]
+
     run = neptune.init(
         project="caplab/net-transform-train-setting",
         api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI0MDQyMjIyZC1mYWQ0LTQ3OWYtYmY1Ny0yMmZlNzA0ODg5NzkifQ==",
@@ -60,36 +72,41 @@ if __name__ == '__main__':
                         help='resume from checkpoint')
     args = parser.parse_args()
 
-    if FROM_SCRATCH:  # train from scratch => todo : cifar 에서 별 차이 없으면 Imagenet-100 으로 가자
-        net = MobileNetV2(cfg=BASE_CFG).to(device)
+    # if FROM_SCRATCH:  # train from scratch => todo : cifar 에서 별 차이 없으면 Imagenet-100 으로 가자
+    # else:
+    #
+    #     # net = MobileNetV2(cfg=CFG).to(device)
 
-    else:
-        # target cfg
-        pass
-        # CFG = [
-        #     (1, 16, 1, 1),
-        #     (6, 24, 2, 1),
-        #     (6, 32, 3, 2), #16->32 , 2->3
-        #     (6, 48, 5, 2), #32->64 , 4->5
-        #     (6, 96, 4, 1), #48->96 , 3->4
-        #     (6, 192, 3, 2), #96->192, 2->3
-        #     (6, 380, 1, 1)]
-        #
-        #
-        # net = MobileNetV2(cfg=CFG).to(device)
-
+    net = MobileNetV2(cfg=BASE_CFG).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr,
                           momentum=0.9, weight_decay=5e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
-    for epoch in range(1, 201):
-        # if epoch % 25 == 0:
-        #     net = get_next_net(net, plan, epoch)
+    if args.resume:
+        # Load checkpoint.
+        print('==> Resuming from checkpoint..')
+        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+        checkpoint = torch.load('./checkpoint/base_ckpt.pth')
+        net.load_state_dict(checkpoint['net'])
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+        print("loaded the model")
 
+    plan_idx = "base"
+    interval = total_epoch // len(PLAN)
+    for epoch in range(start_epoch, total_epoch):
+        if epoch % interval == 0:
+            plan_idx = epoch // interval - 1
+            net = get_next_net(net, PLAN, plan_idx).to(device)
+            optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                                  momentum=0.9, weight_decay=5e-4)
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+            print("=" * 100 + "\n", f"net changed! {PLAN[plan_idx]}" + "\n", "=" * 100)
 
         train(net, criterion, optimizer, trainloader, epoch, device, run)
-        test(net, criterion, testloader, epoch, device, run, "base")
+        test(net, criterion, testloader, epoch, device, run, f"{'_'.join(map(str, PLAN[plan_idx]))}")
         scheduler.step()
 
     run.stop()
