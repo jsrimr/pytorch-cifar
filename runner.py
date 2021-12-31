135c79278a4699f676d641043a694dcdc437dd35
root <soomilee96@naver.com>
2022-01-01 01:03:06 +0900

    noise_added

--- runner.py
+++ runner.py
@@ -15,6 +15,8 @@ from utils import progress_bar
 from models.mobile import MobileNetV2
 from models.mobilenetv2_wider import wider_MobileNetV2
 from models.mobilenetv2_deeper import deeper_MobileNetV2
+import neptune.new as neptune
+import numpy as np
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -25,6 +27,12 @@ args = parser.parse_args()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+
+run = neptune.init(
+        project="caplab/net-transform-train-setting",
+        api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI5ODE0YWMzMC02MWE2LTQ4MmUtODQ0MS0yZWM1NGI3MDYwNzQifQ==",
+    )
 
 # Data
 print('==> Preparing data..')
@@ -85,6 +93,10 @@ def net_transform_wider(net, wider):
                 if (diff > 0):
                     new2 = changed[perm, :, :, :]
                     changed = torch.cat((changed, new2), axis=0)
+                
+                ## NOISE
+                noise = torch.Tensor((0.001)*np.random.normal(0,1,changed.shape)).cuda()
+                changed = changed + noise
 
                 wider.state_dict()[param_tensor].copy_(changed)
                 
@@ -102,6 +114,11 @@ def net_transform_wider(net, wider):
                         cop = (net.state_dict()[param_tensor]).clone().detach()
                         new = cop.data[perm3]
                         toadd = torch.cat((cop, new), axis=0)
+
+                        ## NOISE
+                        noise2 = torch.Tensor((0.001)*np.random.normal(0,1,toadd.shape)).cuda()
+                        toadd = toadd + noise2
+
                         wider.state_dict()[param_tensor].copy_(toadd)
                         third = 1
                     else:
@@ -120,6 +137,9 @@ def net_transform_wider(net, wider):
                             changed = torch.cat((changed, new2), axis=0)
                         else: #last 
                             third = 0
+                        ##NOISE
+                        noise3 = torch.Tensor((0.001)*np.random.normal(0,1,changed.shape)).cuda()
+                        changed = changed + noise3
                         wider.state_dict()[param_tensor].copy_(changed)
                     curr_out = c_out
                     
@@ -133,13 +153,19 @@ def net_transform_wider(net, wider):
                     if (diff > 0):
                         new2 = changed[perm3, :, :, :]
                         changed = torch.cat((changed, new2), axis=0)
-
+                    
+                    ##NOISE
+                    noise4 = torch.Tensor((0.001)*np.random.normal(0,1,changed.shape)).cuda()
+                    changed = changed + noise4
                     wider.state_dict()[param_tensor].copy_(changed)
 
             elif("conv2" in param_tensor):
                 cop3 = (net.state_dict()[param_tensor]).clone().detach()
                 new3 = cop3[perm, :, :, :]
                 toadd3 = torch.cat((cop3, new3), axis=0)
+                ##NOISE
+                noise5 = torch.Tensor((0.001)*np.random.normal(0,1,toadd3.shape)).cuda()
+                toadd3 = toadd3 + noise5
                 wider.state_dict()[param_tensor].copy_(toadd3)
 
             else :
@@ -171,15 +197,18 @@ def net_transform_wider(net, wider):
                     new = cop.data[perm3]
                 #print(param_tensor)
                 toadd = torch.cat((cop, new), axis=0)
+                ##NOISE
+                noise6 = torch.Tensor((0.001)*np.random.normal(0,1,toadd.shape)).cuda()
+                toadd = toadd + noise6
                 wider.state_dict()[param_tensor].copy_(toadd)
                     
     return 
 
 ncfg= [(1,  16, 1, 1),
-       (6,  28, 2, 1),  # NOTE: change stride 2 -> 1 for CIFAR10
-       (6,  40, 3, 2),
-       (6,  64, 4, 2),
-       (6,  96, 3, 1),
+       (6,  24, 2, 1),  # NOTE: change stride 2 -> 1 for CIFAR10
+       (6,  36, 3, 2),
+       (6,  72, 4, 2),
+       (6,  108, 3, 1),
        (6, 180, 3, 2),
        (6, 320, 1, 1)]
 
@@ -202,7 +231,7 @@ if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    checkpoint = torch.load('./checkpoint/ckpt06_middle.pth')
     net.load_state_dict(checkpoint['net'])
     #for param_tensor in net.state_dict():
         #print(param_tensor, "\t", net.state_dict()[param_tensor].size())
@@ -213,14 +242,44 @@ if args.resume:
     print(best_acc)
     print(start_epoch)
 
-
-
+"""
+print('==> Resuming from checkpoint..')
+assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+checkpoint = torch.load('./checkpoint/ckpt.pth')
+net.load_state_dict(checkpoint['net'])    
+best_acc = checkpoint['acc']
+start_epoch = checkpoint['epoch']
+"""
+### Change this part as well
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
-def test(nt):
+# Training
+def train(epoch, nt = net):
+    print('\nEpoch: %d' % epoch)
+    nt.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = nt(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+def test(epoch, nt):
     global best_acc
     nt.eval()
     test_loss = 0
@@ -237,9 +296,37 @@ def test(nt):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
+            run["train/loss"].log(loss.item())
+            run["train/acc"].log(100. * correct / total)
+
             progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': nt.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt08_150epoch.pth')
+        best_acc = acc
 
-test(net)
+#test(0, net)
 new_weights = net_transform_wider(net, wider_net)
-test(wider_net)
+#test(0, wider_net)
+
+
+
+for epoch in range(start_epoch, start_epoch+150):
+    train(epoch, nt = net)
+    test(epoch, net)
+    scheduler.step()
+
+## 1. Change The Loop above
+## 2. Change optimizer etc
+## 3. Change checkpoint name
+## 4. network_transform_wider
